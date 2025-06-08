8a9c61c33425e7039e8e207d665e705583e3e4f1
Asmit-727 <asmit.thapa111@gmail/com>
2025-06-08 19:59:25 +0545

    Update Capitalizer.java to allow user input

--- Capitalizer.java
+++ Capitalizer.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Capitalizer {
     public static void main(String[] args) {
-        String word = "cat";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String word = scanner.nextLine();
 
         String allUpper = word.toUpperCase();  // CAT
         String firstCapital = word.substring(0, 1).toUpperCase() + word.substring(1);  // Cat
@@ -8,5 +13,7 @@ public class Capitalizer {
         System.out.println("Original: " + word);
         System.out.println("All Uppercase: " + allUpper);
         System.out.println("First Letter Capital: " + firstCapital);
+
+        scanner.close();
     }
 }
